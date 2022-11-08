7ede111c6c49fc1bed6e070a0e6d609a2e0f8b16
DDariasP <ddariasp@gmail.com>
2022-11-08 14:01:20 +0100

    nuevo método indexarQRY()

--- src/main/java/com/mb/solrj/APISolrj.java
+++ src/main/java/com/mb/solrj/APISolrj.java
@@ -7,8 +7,6 @@ package com.mb.solrj;
 import java.io.*;
 import java.util.*;
 import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import org.apache.solr.client.solrj.SolrClient;
@@ -96,27 +94,85 @@ public class APISolrj {
         } catch (IOException ex) {
             System.out.println("Error en Scanner.");
         }
-        System.out.println("Documentos indexados en " + collection + ".");
+        System.out.println("\nDocumentos indexados en " + collection + ".\n");
     }
 
-    public static void consultar(String collection, String words) {
+    public static Queue<String> indexarQRY(String filename) {
+
+        Queue<String> docs = new LinkedList<>();
+        Queue<String> words = new LinkedList<>();
+        String line, field;
+        String[] tokens;
+        String total = "";
+        Pattern pattern;
+        Matcher matcher;
+        try {
+            Scanner scan = new Scanner(new File(filename));
+            boolean found = false;
+            while (!found) {
+                line = scan.nextLine();
+                if (line.trim().length() > 0) {
+                    tokens = line.split("\\s+");
+                    if (!".I".equals(tokens[0])) {
+                        total = total + "\n" + line;
+                    } else {
+                        total = total + ".X";
+                    }
+                    if (".I 58".equals(line)) {
+                        found = true;
+                    }
+                }
+            }
+            pattern = Pattern.compile("(\\.W\n)(.*?)(\\.X)", Pattern.DOTALL);
+            matcher = pattern.matcher(total);
+            while (matcher.find()) {
+                field = matcher.group(2).trim();
+                docs.add(field);
+            }
+            while (!docs.isEmpty()) {
+                field = "";
+                line = docs.remove();
+                tokens = line.split("\\s+");
+                if (tokens.length > 5) {
+                    for (int i = 0; i < 5; i++) {
+                        field = field + " " + tokens[i];
+                    }
+                    words.add(field);
+                }
+            }
+        } catch (IOException ex) {
+            System.out.println("Error en Scanner.");
+        }
+        System.out.println("\nConsultas de " + filename + " indexadas.\n");
+        return words;
+    }
+
+    public static void consultar(String collection, Queue<String> words) {
 
         //Preparing the Solr client
         String urlString = "http://localhost:8983/solr/" + collection;
         SolrClient client = new HttpSolrClient.Builder(urlString).build();
-        
+
         final SolrQuery query = new SolrQuery();
         QueryResponse rsp = new QueryResponse();
         SolrDocumentList docs = new SolrDocumentList();
+        String line;
+
+        System.out.println("\nResultados de las consultas en " + collection + ":");
+
         try {
-            query.setQuery(words);
-            //query.setQuery("Apple");
-            //query.addFilterQuery("cat:electronics");
-            //query.setFields("id","price","merchant","cat","store");
-            rsp = client.query(query);
-            docs = rsp.getResults();
-            for (int i = 0; i < docs.size(); ++i) {
-                System.out.println(docs.get(i));
+            while (!words.isEmpty()) {
+                line = words.remove();
+                System.out.println("\n" + line);
+                query.setQuery("text:" + line);
+                //query.setQuery("Apple");
+                //query.addFilterQuery("cat:electronics");
+                query.setFields("ndoc","author","score");
+                rsp = client.query(query);
+                docs = rsp.getResults();
+                for (int i = 0; i < docs.size(); ++i) {
+                    System.out.println(docs.get(i));
+                }
             }
         } catch (SolrServerException ex) {
             System.out.println("Error en Solrj.");
@@ -138,7 +194,7 @@ public class APISolrj {
             //Deleting the documents from Solr
             Solr.deleteByQuery("*");
             Solr.commit();
-            System.out.println("Documentos de " + collection + " borrados.");
+            System.out.println("\nDocumentos de " + collection + " borrados.\n");
         } catch (SolrServerException ex) {
             System.out.println("Error en Solrj.");
         } catch (IOException ex) {
--- src/main/java/com/mb/solrj/Solrj.java
+++ src/main/java/com/mb/solrj/Solrj.java
@@ -2,12 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
  */
-
 package com.mb.solrj;
 
 import static com.mb.solrj.APISolrj.borrarTodo;
 import static com.mb.solrj.APISolrj.consultar;
 import static com.mb.solrj.APISolrj.indexarCISI;
+import static com.mb.solrj.APISolrj.indexarQRY;
+import java.util.Queue;
 
 /**
  *
@@ -16,10 +17,11 @@ import static com.mb.solrj.APISolrj.indexarCISI;
 public class Solrj {
 
     public static void main(String[] args) {
-        String archivo = ".\\collection\\CISI";
-        String colecion = "pruebas";
+        String archivo = ".\\collection\\CISI.QRY";
+        String coleccion = "CISI";
 
-        consultar(colecion,"*:*");
+        Queue<String> consultas = indexarQRY(archivo);
+        consultar(coleccion,consultas);
 
     }
 }
